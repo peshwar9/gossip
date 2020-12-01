9be52d5ff182349e9fb4b26d01d9d9b25320eb92
peshwar9 <par.esh1937@gmail.com>
2020-12-01 23:12:21 +0530

    added notification message type

--- src/gossip.rs
+++ src/gossip.rs
@@ -1,12 +1,17 @@
 use std::str;
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Peer {
     pub host: String,
     pub port: u16,
 }
-
 #[derive(Debug)]
+pub struct Payload {
+    pub sender: String,
+    pub peer: Peer,
+}
+
+#[derive(Debug, Clone)]
 pub struct PeerList {
     list: Vec<Peer>,
 }
@@ -14,6 +19,7 @@ pub struct PeerList {
 #[derive(Debug)]
 pub enum GossipMessage {
     Join(Peer),
+    NotifyNew(Payload),
     //   List(PeerList),
     //   RandomMessage(String),
 }
@@ -22,19 +28,35 @@ impl GossipMessage {
     pub fn as_bytes(self) -> String {
         match self {
             GossipMessage::Join(peer) => format!("Join {} {}\n", peer.host, peer.port),
+            GossipMessage::NotifyNew(node) => {
+                format!("Joined {} {}\n", node.peer.host, node.peer.port)
+            }
         }
     }
-    pub fn from_bytes(buf: &[u8]) -> Option<GossipMessage> {
+    pub fn from_bytes(buf: &[u8], port: u16) -> Option<GossipMessage> {
         let msg = String::from_utf8_lossy(buf);
+        let msg = msg.lines().next().unwrap();
+     //   println!("Message received is {}",msg);
         let mut msg_iter = msg.split_ascii_whitespace();
         let msg_type = msg_iter.next().unwrap();
-
+        let mut sender: String = "".into();
+        if msg_type == "Joined" {
+            sender = msg_iter.next().unwrap().into();
+        }
+       
         let host = msg_iter.next().unwrap();
         let port = msg_iter.next().unwrap();
         match msg_type {
             "Join" => Some(GossipMessage::Join(Peer {
                 host: host.into(),
                 port: port.parse().unwrap(),
+            })),
+            "Joined" => Some(GossipMessage::NotifyNew(Payload {
+                sender: sender.into(),
+                peer: Peer {
+                    host: host.into(),
+                    port: port.parse().unwrap(),
+                },
             })),
             _ => None,
         }
--- src/main.rs
+++ src/main.rs
@@ -20,7 +20,8 @@ fn main() {
     let opt = Opt::from_args();
     // Capture sys time
     let srv_start_time = SystemTime::now();
-
+    // Accumulate list of peers
+    let mut peer_list: Vec<String> = vec![];
     // Create a UDP socket server binding to port from command-line parameter: port,
     let udp_socket =
         UdpSocket::bind(format!("127.0.0.1:{}", opt.port)).expect("Unable to bind to port");
@@ -30,20 +31,49 @@ fn main() {
     if let Some(remote_peer) = opt.connect {
         let send_buffer = format!("Join localhost {}\n", opt.port);
         let addr = remote_peer.to_socket_addrs().unwrap().next().unwrap();
-        println!("Going to {}, following: {:?}", addr, send_buffer);
+//        println!("Going to {}, following: {:?}", addr, send_buffer);
         let _ = udp_socket.send_to(send_buffer.as_bytes(), addr);
+        peer_list.push(format!("localhost:{}", opt.port));
     }
 
     let mut buffer = [0; 1024];
     loop {
         let socket_new = udp_socket.try_clone().expect("Unable to clone socket");
+        let peer_list_new = peer_list.clone();
         match socket_new.recv_from(&mut buffer) {
-            Ok((_, _)) => {
-                let msg = GossipMessage::from_bytes(&buffer).unwrap();
+            Ok(_) => {
+                let msg = GossipMessage::from_bytes(&buffer, opt.port).unwrap();
+             //   println!("Got message from socket: {:?}",msg);
                 match msg {
                     GossipMessage::Join(peer) => {
-                        println!("Join message from {}:{}", peer.host, peer.port)
+                        println!("Join message from {}:{}", peer.host, peer.port);
+                        // Add the sender to peer list
+                        let addr = format!("{}:{}", peer.host, peer.port);
+                        let remote_addr = addr.to_socket_addrs().unwrap().next().unwrap();
+                        peer_list.push(format!("localhost:{}", peer.port));
+                        // Notify peers in list of new joinee
+                        for node in peer_list.clone() {
+                            // Don't send message back to sender
+                            if node != format!("localhost:{}", peer.port) {
+                                let send_buffer = format!(
+                                    "Joined localhost:{} localhost {}\n",
+                                    opt.port, peer.port
+                                );
+                                let _ = udp_socket.send_to(send_buffer.as_bytes(), node);
+                            }
+                        }
                     }
+                                        GossipMessage::NotifyNew(node) => {
+                        // Add the newly joined node to peer list
+                        let addr = format!("{}:{}", node.peer.host, node.peer.port);
+                        let remote_addr = addr.to_socket_addrs().unwrap().next().unwrap();
+                        peer_list.push(format!("localhost:{}", node.peer.port));
+
+                        println!(
+                            "Notification message from {}: localhost {} Joined",
+                            node.sender, node.peer.port
+                        )
+                    },
                     _ => println!("Got invalid gossip message"),
                 }
             }
